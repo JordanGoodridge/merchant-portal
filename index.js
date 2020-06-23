d70a03bab777ee0bae5347f4bfaf973f6ef7e963
JordanGoodridge <jordangoodridge8@yahoo.com>
2020-06-23 17:25:06 -0600

    connection string fix

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 3000;
+var heroku_config = process.env.DATABASE_URL || "postgres://aczmbwlkixdtfq:e80bb0c9f8fcab36b3712ddbd4fe356981174d9c930c0f8c0202ae5c1165297e@ec2-3-216-129-140.compute-1.amazonaws.com:5432/d541m4me20fera";
+
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
@@ -11,7 +13,7 @@ app.listen(PORT, () => {
 });
 
 var pg = require('pg');
-var pool = new pg.Pool();
+var pool = new pg.Pool(heroku_config);
 
 pool.connect(process.env.DATABASE_URL, function(err, client, done) {
   client.query('SELECT * FROM merchant', function(err, result) {
