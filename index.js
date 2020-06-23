7495e8cd46cdcf583dfcebb4a3cc1ae66a8f872c
JordanGoodridge <jordangoodridge8@yahoo.com>
2020-06-23 17:34:00 -0600

    client

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 3000;
-var heroku_config = process.env.DATABASE_URL || "postgres://aczmbwlkixdtfq:e80bb0c9f8fcab36b3712ddbd4fe356981174d9c930c0f8c0202ae5c1165297e@ec2-3-216-129-140.compute-1.amazonaws.com:5432/d541m4me20fera";
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT * FROM merchant;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
@@ -12,14 +29,5 @@ app.listen(PORT, () => {
  console.log("Server running on port " + PORT);
 });
 
-var pg = require('pg');
-var pool = new pg.Pool(heroku_config);
 
-pool.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query('SELECT * FROM merchant', function(err, result) {
-    done();
-    if(err) return console.error(err);
-    console.log(result.rows);
-  });
-});
 
