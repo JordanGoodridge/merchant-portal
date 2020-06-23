f7253beaa1a2c0fe04064fc20b4be95e26cc1b50
JordanGoodridge <jordangoodridge8@yahoo.com>
2020-06-23 15:29:30 -0600

    database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -8,22 +9,18 @@ const pool = new Pool({
   }
 });
 
-app.get('/db', async (req, res) => {
+app.get('/', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
+      const result = await client.query('SELECT * FROM merchant');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
+      res.render('pages/', results );
       client.release();
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
     }
   })
-
-app.get("/", (req, res, next) => {
- res.json(["Tony"]);
-});
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
