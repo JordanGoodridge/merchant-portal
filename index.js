118e9520c9d189072e8f3eb47403077c3b4d31e1
JordanGoodridge <jordangoodridge8@yahoo.com>
2020-06-23 15:48:20 -0600

    database fix

--- index.js
+++ index.js
@@ -2,26 +2,24 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-const { Pool } = require('pg');
-const pool = new Pool({
+const { Client } = require('pg');
+
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-app.get('/', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM merchant');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 app.listen(PORT, () => {
  console.log("Server running on port ${ PORT }");
