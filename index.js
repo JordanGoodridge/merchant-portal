3bd08625974957e1a7602848b85fd32643c7da9c
JordanGoodridge <jordangoodridge8@yahoo.com>
2020-06-23 15:53:18 -0600

    db

--- index.js
+++ index.js
@@ -2,25 +2,20 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
 app.listen(PORT, () => {
- console.log("Server running on port ${ PORT }");
+ console.log("Server running on port " + PORT);
+});
+
+var pg = require('pg');
+
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('SELECT * FROM merchant', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
 });
