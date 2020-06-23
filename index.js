866a2432ee9f660db1e740ee519192b5e206a566
JordanGoodridge <jordangoodridge8@yahoo.com>
2020-06-23 14:36:36 -0600

    datbase connection

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var express = require("express");
+const { Client } = require('pg');
 var app = express();
-connectionString = {
-connectionString: process.env.DATABASE_URL,
-ssl: true
-};
+
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 
 app.get("/", (req, res, next) => {
