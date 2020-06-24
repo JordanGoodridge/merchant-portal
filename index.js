ec4eea645aadc5bdec2845101f7065c0b632651e
JordanGoodridge <jordangoodridge8@yahoo.com>
2020-06-24 14:03:51 -0600

    create merchant user

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.get('/', function(request, response) {
 app.post("/merchant-signup", function(request, response) {
 	var new_merchant = request.body;
 	console.log(request.body.email);
+	client.connect();
 
 	client.query("INSERT INTO merchant (name, longitude, latitude, email, password) VALUES" + request.body.store_name + "," + request.body.longitude + "," + request.body.latitude + "," + request.body.email + "," + request.body.password + ";", (err, res) => {
   	if (err) throw err;
