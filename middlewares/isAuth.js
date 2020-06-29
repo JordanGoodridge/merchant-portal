b5ad0a3afab531e2c74a4f7dfbcecaf0a30cb750
flaskur <flaskur@gmail.com>
2020-06-29 14:52:57 -0400

    added isAuth middleware to read jwt

---
+++ middlewares/isAuth.js
@@ -0,0 +1,24 @@
+import { verify } from 'jsonwebtoken';
+
+export const isAuth = (request, response, next) => {
+	const givenToken = request.headers.authorization;
+
+	if (!givenToken) {
+		return response.json({ success: false, errorMessage: 'NO TOKEN' });
+	}
+
+	let verifiedToken;
+	try {
+		verifiedToken = verify(givenToken, 'secret');
+	} catch (error) {
+		return response.json({ success: false, errorMessage: 'PROBLEM VERIFYING TOKEN' });
+	}
+
+	if (!verifiedToken) {
+		return response.json({ success: false, errorMessage: 'BAD TOKEN' });
+	}
+
+	request.email = verifiedToken.email;
+
+	next();
+};
