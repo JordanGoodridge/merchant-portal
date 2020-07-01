591ccbd6c7eb4f6dd602468cfcd653a85efcdcdd
flaskur <flaskur@gmail.com>
2020-07-01 16:49:15 -0400

    testing isAuth again bearer split

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -3,12 +3,10 @@ var cybersourceRestApi = require('cybersource-rest-client');
 var path = require('path');
 var filePath = path.resolve('Data/Configuration.js');
 var configuration = require(filePath);
-const {verify} = require('jsonwebtoken')
+const { verify } = require('jsonwebtoken');
 const client = require('../database/client');
 
-
-function config(MerchantId, MerchantKeyId, MerchantSecretKey){
-
+function config(MerchantId, MerchantKeyId, MerchantSecretKey) {
 	const AuthenticationType = 'http_signature';
 	const RunEnvironment = 'cybersource.environment.SANDBOX';
 
@@ -24,52 +22,73 @@ function config(MerchantId, MerchantKeyId, MerchantSecretKey){
 	const LogDirectory = '../log';
 	const LogfileMaxSize = '5242880'; //10 MB In Bytes
 	var configObj = {
-		'authenticationType': AuthenticationType,	
-		'runEnvironment': RunEnvironment,
+		authenticationType: AuthenticationType,
+		runEnvironment: RunEnvironment,
 
-		'merchantID': MerchantId,
-		'merchantKeyId': MerchantKeyId,
-		'merchantsecretKey': MerchantSecretKey,
-		
-		'keyAlias': KeyAlias,
-		'keyPass': KeyPass,
-		'keyFileName': KeyFileName,
-		'keysDirectory': KeysDirectory,
-		
-		'enableLog': EnableLog,
-		'logFilename': LogFileName,
-		'logDirectory': LogDirectory,
-		'logFileMaxSize': LogfileMaxSize
+		merchantID: MerchantId,
+		merchantKeyId: MerchantKeyId,
+		merchantsecretKey: MerchantSecretKey,
+
+		keyAlias: KeyAlias,
+		keyPass: KeyPass,
+		keyFileName: KeyFileName,
+		keysDirectory: KeysDirectory,
+
+		enableLog: EnableLog,
+		logFilename: LogFileName,
+		logDirectory: LogDirectory,
+		logFileMaxSize: LogfileMaxSize
 	};
 	return configObj;
 }
 
-
 const postCheckout = async (request, resp) => {
-    try {
+	const email = request.email;
 
-        console.log("Request: " + request);
-        const { cart, number, expirationMonth, expirationYear, securityCode, totalAmount,currency, firstName, lastName, address1, address2, locality, administrativeArea, postalCode, country, email, phoneNumber, merch_id  } = request.body;
-		console.log("Checkout request with:")
-		console.log("CC:"+number);
-        console.log("Expiration"+ expirationMonth +"/" + expirationYear)
-		console.log("Amount" + totalAmount + " " +currency)
-        console.log("Name: " + firstName+ " " + lastName)
-		console.log("Address:" + address1 + " " + locality + " " + administrativeArea+ " " + postalCode + " " + country)
-		console.log("Phone: " + phoneNumber)
-		console.log("Email: " + email)
-		console.log("MerchID: " +merch_id)
+	try {
+		console.log('Request: ' + request);
+		const {
+			cart,
+			number,
+			expirationMonth,
+			expirationYear,
+			securityCode,
+			totalAmount,
+			currency,
+			firstName,
+			lastName,
+			address1,
+			address2,
+			locality,
+			administrativeArea,
+			postalCode,
+			country,
+			email,
+			phoneNumber,
+			merch_id
+		} = request.body;
+		console.log('Checkout request with:');
+		console.log('CC:' + number);
+		console.log('Expiration' + expirationMonth + '/' + expirationYear);
+		console.log('Amount' + totalAmount + ' ' + currency);
+		console.log('Name: ' + firstName + ' ' + lastName);
+		console.log(
+			'Address:' + address1 + ' ' + locality + ' ' + administrativeArea + ' ' + postalCode + ' ' + country
+		);
+		console.log('Phone: ' + phoneNumber);
+		console.log('Email: ' + email);
+		console.log('MerchID: ' + merch_id);
 		console.log(cart);
-		
-		const query_feedback = await client.query("SELECT * FROM merchant WHERE merchant.merch_id = '" + request.body.merch_id + "' ;");
+
+		const query_feedback = await client.query(
+			"SELECT * FROM merchant WHERE merchant.merch_id = '" + request.body.merch_id + "' ;"
+		);
 		var key_id = query_feedback.rows[0].key_id;
 		var visa_merchant_id = query_feedback.rows[0].visa_merchant_id;
 		var shared_key = query_feedback.rows[0].shared_key;
 		var configObject = config(visa_merchant_id, key_id, shared_key);
 
-
-
-        var enable_capture = true;
+		var enable_capture = true;
 
 		// var configObject = new configuration();
 		var apiClient = new cybersourceRestApi.ApiClient();
@@ -119,33 +138,29 @@ const postCheckout = async (request, resp) => {
 
 		requestObj.orderInformation = orderInformation;
 
-    
-
 		var instance = new cybersourceRestApi.PaymentsApi(configObject, apiClient);
 		var cap_instance = new cybersourceRestApi.CaptureApi(configObject, apiClient);
 
-		instance.createPayment(requestObj, function (error, data, response) {
+		instance.createPayment(requestObj, function(error, data, response) {
 			if (error) {
 				console.log('\nError : ' + JSON.stringify(error));
-			}
-			else if (data) {
+			} else if (data) {
 				var id = data['id'];
 				console.log('\n*************** Capture Payment *********************');
 				console.log('Payment ID passing to capturePayment : ' + id);
 
-				cap_instance.capturePayment(requestObj, id, function (error, data, response) {
+				cap_instance.capturePayment(requestObj, id, function(error, data, response) {
 					if (error) {
 						console.log('\nError : ' + JSON.stringify(error));
-					}
-					else if (data) {
-						console.log("Capture Payment sucess: \n");
+					} else if (data) {
+						console.log('Capture Payment sucess: \n');
 						console.log('\nData : ' + JSON.stringify(data));
 					}
 
 					console.log('\nResponse : ' + JSON.stringify(response));
-                    console.log('\nResponse Code of Capture a Payment : ' + JSON.stringify(response['status']));
+					console.log('\nResponse Code of Capture a Payment : ' + JSON.stringify(response['status']));
 
-                        resp.json({ success: true });
+					resp.json({ success: true });
 
 					callback(error, data, response);
 				});
@@ -153,35 +168,44 @@ const postCheckout = async (request, resp) => {
 
 			console.log('\nResponse : ' + JSON.stringify(response));
 			console.log('\nResponse Code of Process a Payment : ' + JSON.stringify(response['status']));
-            resp.json({ success: true });
+			resp.json({ success: true });
 
-            callback(error, data, response);
-            resp.json({ success: true });
-
+			callback(error, data, response);
+			resp.json({ success: true });
 		});
-		const new_order = await client.query("INSERT INTO orders (cust_id, order_date, status, merch_id) VALUES(" + request.body.cust_id + ", CURRENT_TIMESTAMP, TRUE, "+ request.body.merch_id+") ;");
-        const new_order_results = await client.query("SELECT orders.order_id FROM orders WHERE orders.cust_id = " + request.body.cust_id + " ORDER BY orders.order_id DESC LIMIT 1;");
+		const new_order = await client.query(
+			'INSERT INTO orders (cust_id, order_date, status, merch_id) VALUES(' +
+				request.body.cust_id +
+				', CURRENT_TIMESTAMP, TRUE, ' +
+				request.body.merch_id +
+				') ;'
+		);
+		const new_order_results = await client.query(
+			'SELECT orders.order_id FROM orders WHERE orders.cust_id = ' +
+				request.body.cust_id +
+				' ORDER BY orders.order_id DESC LIMIT 1;'
+		);
 		var new_order_id = new_order_results.rows[0].order_id;
 		console.log(new_order_id);
-        for (var id = 0; id < request.body.cart.length; id++){
-			console.log("Adding order: ")
-			console.log(request.body.cart[id].itemId )
-			console.log(request.body.cart[id].quantity)
-			client.query("INSERT INTO orderitems (item_id, order_id, quantity) VALUES ( "+ request.body.cart[id].itemId + ","+  new_order_id + ","+request.body.cart[id].quantity +");");
-        }
+		for (var id = 0; id < request.body.cart.length; id++) {
+			console.log('Adding order: ');
+			console.log(request.body.cart[id].itemId);
+			console.log(request.body.cart[id].quantity);
+			client.query(
+				'INSERT INTO orderitems (item_id, order_id, quantity) VALUES ( ' +
+					request.body.cart[id].itemId +
+					',' +
+					new_order_id +
+					',' +
+					request.body.cart[id].quantity +
+					');'
+			);
+		}
+	} catch (error) {
+		console.log('\nException on calling the API : ' + error);
+		resp.json({ sucess: false });
 	}
-	catch (error) {
-        console.log('\nException on calling the API : ' + error);
-        resp.json({sucess:false})
-	}
-
-
 };
-
-
-
-
-
 
 // module.exports.paymentRouter = simple_authorization_internet;
 module.exports.postCheckout = postCheckout;
--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,20 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 const isAuth = (request, response, next) => {
-	const givenToken = request.headers.authorization;
+	let givenToken = request.headers.authorization.split(' ')[1];
+
 	console.log(givenToken);
+	console.log(typeof givenToken);
 	console.log('token is', JSON.stringify(givenToken));
 
 	if (!givenToken) {
 		return response.json({ success: false, errorMessage: 'NO TOKEN' });
 	}
 
-	const verifiedToken = jwt.verify(givenToken, 'secret');
-	// try {
-	// 	verifiedToken = jwt.verify(givenToken, 'secret');
-	// } catch (error) {
-	// 	return response.json({ success: false, errorMessage: 'PROBLEM VERIFYING TOKEN' });
-	// }
+	let verifiedToken;
+	try {
+		verifiedToken = jwt.verify(givenToken, 'secret');
+	} catch (error) {
+		return response.json({ success: false, errorMessage: 'PROBLEM VERIFYING TOKEN' });
+	}
 
 	if (!verifiedToken) {
 		return response.json({ success: false, errorMessage: 'BAD TOKEN' });
